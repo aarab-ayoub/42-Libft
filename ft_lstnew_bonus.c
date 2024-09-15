3a99191a21baa7c133fee1218c69f0fa9e1585ff
ayoub <ayoubaarab571@gmail.com>
2024-09-15 01:05:16 +0100

    bonus

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->next = NULL;
+	node->content = content;
+	return (node);
+}
